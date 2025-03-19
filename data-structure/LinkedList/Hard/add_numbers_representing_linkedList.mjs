aed5f96bec3b9cf6bd6e56e6f55292c9f7a092cc
EhsanEsmaily747 <ehsanesmaily747@gmail.com>
2025-03-20 00:21:39 +0430

    add numbers representing linked list function implemented using adding inside linkedlist itself

--- data-structure/LinkedList/Hard/add_numbers_representing_linkedList.mjs
+++ data-structure/LinkedList/Hard/add_numbers_representing_linkedList.mjs
@@ -1,63 +1,51 @@
-import {LinkedList} from '../Easy/linkedList.mjs'
-import {insertNode} from '../Easy/insert_node.mjs'
-import printList from '../Easy/print_linkedList.mjs'
+import { LinkedList,Node } from "../Easy/linkedList.mjs";
+import { insertNode } from "../Easy/insert_node.mjs";
+import printList from "../Easy/print_linkedList.mjs";
 
-const list1 = new LinkedList()
-const list2 = new LinkedList()
+const list1 = new LinkedList();
+const list2 = new LinkedList();
 
-insertNode(8, list1)
-insertNode(2, list1)
-insertNode(3, list1)
+insertNode(8, list1);
+insertNode(2, list1);
+insertNode(3, list1);
 
-insertNode(2, list2)
-insertNode(3, list2)
-insertNode(8, list2)
+insertNode(2, list2);
+insertNode(3, list2);
+insertNode(8, list2);
 
-printList(list1)
-printList(list2)
+printList(list1);
+printList(list2);
 
 // Write a function to add two numbers represented as linked lists.
 
-
 function addLinkedlistNumbers(listA, listB) {
-    let first_node_numbers = ''
-    let second_node_numbers = ''
-    let result = 0
-    let nodeA = listA.head
-    let nodeB = listB.head
+  let nodeA = listA.head;
+  let nodeB = listB.head;
+  let carry = 0;
+  let tail = null;
+  const result = new LinkedList();
 
-    if(listA?.size === listB?.size){
-        while(nodeA){
-            first_node_numbers += nodeA.value
-            second_node_numbers += nodeB.value
-            nodeA = nodeA.next
-            nodeB = nodeB.next
-        }
+  while (nodeA || nodeB || carry) {
+    const value1 = nodeA ? nodeA.value : 0;
+    const value2 = nodeB ? nodeB.value : 0;
+
+    const sum = value1 + value2 + carry;
+    carry = Math.floor(sum / 10);
+    const newNode = new Node(sum % 10);
+  
+    if(!result.head){
+        result.head = newNode
     }else{
-        while(nodeA || nodeB){
-            if (nodeA && nodeB) {
-                first_node_numbers += nodeA.value
-                second_node_numbers += nodeB.value
-                nodeA = nodeA.next
-                nodeB = nodeB.next
-            }else if (nodeA) {
-                first_node_numbers += nodeA.value
-                nodeA = nodeA.next
-            }else{
-                second_node_numbers += nodeB.value
-                nodeB = nodeB.next
-            }
-        }
+        tail.next = newNode
     }
-    result = (Number(first_node_numbers.split('').reverse().join(''))+Number(second_node_numbers.split('').reverse().join(''))).toString()
-    const total_linkedList = new LinkedList() 
-    for (let i = 0; i < result.toString().length; i++) {
-        insertNode(result[i], total_linkedList)    
-    }
-    return total_linkedList
+    tail = newNode
+
+    if(nodeA) nodeA = nodeA.next
+    if(nodeB) nodeB = nodeB.next
 }
 
-// printList(addLinkedlistNumbers(list1, list2))
-// 3>2>8>         //823
-// 8>3>2>         //238
-// 1>6>0>1>       //1061
+return result
+}
+
+printList(addLinkedlistNumbers(list1, list2))
+
